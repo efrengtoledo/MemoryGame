c8d8bdea0ee787bb17e9b6f0fb6504ce8086da8b
efrengtoledo <efrengtoledo@lewisu.edu>
2022-04-22 11:51:56 -0500

    Changed css, js, and html files to add more originality

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const clueHoldTime = 1000;
 const cluePauseTime = 333;
 const nextClueWaitTime = 1000;
 // Global Variables 
-var pattern = Array.from({length: 8}, () => Math.floor((Math.random() * 4) + 1)) // Modified, so game picks different pattern each time
+var pattern = Array.from({length: 8}, () => Math.floor((Math.random() * 5) + 1)) // Modified, so game picks different pattern each time
 var progress = 0;
 var gamePlaying = false;
 var tonePlaying = false;
 var volume = 0.5;
 var guessCounter = 0;
+var wrongCount = 0;
 
 function startGame() {
     progress = 0;
@@ -27,12 +28,13 @@ function stopGame() {
 function loseGame() {
     stopGame();
     alert("Game Over, You Lost.");
+    location.reload();
 }
 
 function winGame() {
     stopGame();
     alert("Game Over, You Win!");
-
+    location.reload();
 }
 
 function lightButton(btn) {
@@ -68,13 +70,13 @@ function guess(btn) {
         return;
     }
     // Game Logic goes here
-    if (pattern[guessCounter] == btn) {
-        if (guessCounter == progress) {
-            if (pattern.length - 1 == progress) {
+    if (pattern[guessCounter] == btn) { // Checks button clicked to see if it matches with pattern list at given index
+        if (guessCounter == progress) { // if so, then check if our guess counter matches our progress count. If not, then increment guess count by 1
+            if (pattern.length - 1 == progress) { // If we reach the entire length of the list then we win, then exit
                 winGame();
                 return;
             }
-            else {
+            else { // If length does not match progress, then we increment progress by 1 and add a new clue sequence
                 progress++;
                 playClueSequence();
             }
@@ -83,18 +85,23 @@ function guess(btn) {
             guessCounter++;
         } 
     }
-    else {
-        loseGame();
-        return;
+    else { // If guess is wrong, we add to number of wrong attempts. A red X gets added and concatenated to the empty string on the page
+        wrongCount++;
+        document.getElementById("wrong").innerHTML += 'X';
+        if (wrongCount > 2) { // If three wrong attempts, you lose
+            loseGame();
+            return;
+        }
     } 
 }
 
 // Sound Synthesis Functions
 const freqMap = {
-    1: 261.6,
-    2: 329.6,
-    3: 392,
-    4: 466.2
+    1: 130.8, // C
+    2: 146.8, // D
+    3: 164.8, // E
+    4: 174.6, // F
+    5: 261.6  // C ^
   }
   function playTone(btn,len){ 
     o.frequency.value = freqMap[btn]
